7586d7233992b0270ee5189df127717b56aba727
peterdillon <peter.dillon.biz@gmail.com>
2025-01-12 11:38:11 -0800

    removing firebase db refs

---
+++ .angular/cache/17.0.7/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,24 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-EU6QKSWJ.js";
+import "./chunk-G3UAX3O7.js";
+import "./chunk-NIUUSC6U.js";
+import "./chunk-7AG54Y4B.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-S3ZHAECU.js";
+import "./chunk-AFRS2OIU.js";
+import "./chunk-ENBES6XL.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
